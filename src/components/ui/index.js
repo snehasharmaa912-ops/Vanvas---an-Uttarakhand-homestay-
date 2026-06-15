5a4aefa548035b2906c97bc45a2611431a0a4712
Sneha Sharma <snehasharmaa912@gmail.com>
2026-06-15 11:56:48 +0530

    feat: add component library index with all UI exports

---
+++ src/components/ui/index.js
@@ -0,0 +1,11 @@
+/**
+ * VanaVas UI Component Library
+ * Import all components from here:
+ * import { Button, Input, Modal, Toast, Loader } from '../components/ui'
+ */
+
+export { default as Button } from './Button'
+export { default as Input } from './Input'
+export { default as Modal } from './Modal'
+export { default as Toast } from './Toast'
+export { default as Loader } from './Loader'
