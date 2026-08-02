2c4a4b8dcb2e1364dabe989247b0fdcf585df43f
Sneha Sharma <snehasharmaa912@gmail.com>
2026-08-02 08:39:43 +0530

    refactor: socket.js uses centralized API_BASE_URL instead of hardcoded URL

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
+import { API_BASE_URL } from './api'
 
-const SOCKET_URL = 'https://vanvas-an-uttarakhand-homestay.onrender.com'
-
+const SOCKET_URL = API_BASE_URL
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   transports: ['websocket', 'polling'],
